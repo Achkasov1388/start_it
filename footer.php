f73747c0e651045c03bb6d61ef655341323834a2
achkasovihor94 <achkasovihor94@gmail.com>
2018-12-26 22:33:58 +0200

    2-я неделя

---
+++ footer.php
@@ -0,0 +1,26 @@
+
+
+        <footer id="footer" class="footer">
+            <div class="footer-bottom">
+                <div class="container">
+                    <div class="copyright">
+                        <p> &copy; 2018 - carrby.agency. Created by <a href="index.html#">WP ThemeBooster</a> All rights reserved.</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
+        <!-- All JavaScript Files
+        ================================================== -->
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-3.2.1.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/bootstrap.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.mixitup.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.fancybox.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/owl.carousel.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/typed.min.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/menu.js"></script>
+        <script src="<?php echo get_template_directory_uri(); ?>/js/custom.js"></script>
+
+    </body>
+</html>
---
+++ index.php
@@ -0,0 +1,15 @@
+<?php get_header(); ?>
+
+<?php get_template_part( 'parts/slider' ); ?>
+        
+<div class="main-wrapper">
+
+    <?php get_template_part( 'parts/services' ); ?>
+    <?php get_template_part( 'parts/about' ); ?>
+    <?php get_template_part( 'parts/works' ); ?>
+    <?php get_template_part( 'parts/blog' ); ?>
+    <?php get_template_part( 'parts/contact' ); ?>
+
+</div><!-- End Main Wrapper-->
+
+<?php get_footer(); ?>
