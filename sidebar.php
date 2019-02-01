8ec4a892d593e3b35b5a95a078101739b24b9666
achkasov1388 <achkasovihor94@gmail.com>
2019-02-01 23:01:03 +0200

    5я неделя. Сайдбар

--- sidebar.php
+++ sidebar.php
@@ -2,10 +2,10 @@
 
     <div id="search-2" class="widget widget_search">
         <div class="sidebar_search">
-            <form class="sidebar_search_form">
-                <input type="text" name="search" class="form-control" placeholder="Search">
+            <form method="get" class="sidebar_search_form">
+                <input type="text" name="s" class="form-control" placeholder="Search">
                 <button type="submit" class="form-control form-control-submit"><i
-                        class="ion-ios-search"></i></button>
+                            class="ion-ios-search"></i></button>
             </form>
         </div>
     </div>
@@ -14,27 +14,38 @@
         <h4 class="widget_title"><span>Recent Posts</span></h4>
         <div class="sidebar_recent_posts">
             <ul class="recent_posts_list">
-                <li>
-                    <img src="images/blog/post-thumb-1.jpg" alt="insta">
-                    <div class="post_content">
-                        <h6><a href="blog-single.html#">Get the best quote for your service</a></h6>
-                        <p class="date">08 June, 2018</p>
-                    </div>
-                </li>
-                <li>
-                    <img src="images/blog/post-thumb-2.jpg" alt="insta">
-                    <div class="post_content">
-                        <h6><a href="blog-single.html#">Get the best quote for your service</a></h6>
-                        <p class="date">15 May, 2018</p>
-                    </div>
-                </li>
-                <li>
-                    <img src="images/blog/post-thumb-3.jpg" alt="insta">
-                    <div class="post_content">
-                        <h6><a href="blog-single.html#">Get the best quote for your service</a></h6>
-                        <p class="date">12 April, 2018</p>
-                    </div>
-                </li>
+
+                <?php
+
+                $recent = new WP_Query([
+                    'post_type' => 'post',
+                    'posts_per_page' => 3,
+                ]);
+
+                while ($recent->have_posts()) :
+
+                    $recent->the_post();
+                    setup_postdata($recent->post);
+
+
+                    ?>
+
+                    <li>
+                        <?php the_post_thumbnail(); ?>
+                        <div class="post_content">
+                            <h6><a href="<?php echo get_the_permalink(); ?>"><?php the_title(); ?></a></h6>
+                            <p class="date"><?php the_date('j F, Y'); ?></p>
+                        </div>
+                    </li>
+
+                <?php
+
+                endwhile;
+                wp_reset_postdata();
+
+                ?>
+
+
             </ul>
         </div>
     </div>
@@ -43,10 +54,20 @@
         <h4 class="widget_title"><span>Archives List</span></h4>
         <div class="sidebar_archive">
             <ul class="archive_list">
-                <li><a href="blog-single.html#">December 2017</a></li>
-                <li><a href="blog-single.html#">January 2018</a></li>
-                <li><a href="blog-single.html#">March 2018</a></li>
-                <li><a href="blog-single.html#">June 2018</a></li>
+                <?php
+
+                $args = array(
+                    'type' => 'monthly',
+                    'limit' => '5',
+                    'format' => 'html',
+                    'echo' => 1,
+                    'order' => 'DESC',
+                    'post_type' => 'post'
+                );
+
+                wp_get_archives($args);
+
+                ?>
             </ul>
         </div>
     </div>
@@ -55,10 +76,24 @@
         <h4 class="widget_title"><span>Categories</span></h4>
         <div class="sidebar_categories">
             <ul class="category_list">
-                <li><a href="blog-single.html#">Marketing <span>(5)</span></a></li>
-                <li><a href="blog-single.html#">Consultancy <span>(5)</span></a></li>
-                <li><a href="blog-single.html#">Finance <span>(5)</span></a></li>
-                <li><a href="blog-single.html#">Agency <span>(5)</span></a></li>
+                <?php
+
+                $categories = get_terms([
+                    'taxonomy'      => array( 'category' ), // название таксономии с WP 4.5
+                    'orderby'       => 'count',
+                    'order'         => 'DESC',
+                    'number'        => 5,
+                    'fields'        => 'all',
+                ]);
+
+                foreach ($categories as $term) : ?>
+                    <li>
+                        <a href="<?php echo get_category_link($term); ?>">
+                            <?php echo $term->name ?>
+                            <span>(<?php echo $term->count ?>)</span>
+                        </a>
+                    </li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </div>
